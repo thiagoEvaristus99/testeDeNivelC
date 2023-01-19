bf288717df4db76593c5b6d07ce5ab5710809d3a
Thiago Emanoel <123104885+thiagoEvaristus99@users.noreply.github.com>
2023-01-19 15:17:00 -0300

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h> 
+#include <locale.h>
+
+void inverte(int vetor[], int n){
+	int auxiliar[n];
+	int index;
+	
+	for(index = 0; index < n; index++){
+		auxiliar[index] = vetor[index];
+	}
+
+
+	for(index = 0; index < n; index++){
+		vetor[index] = auxiliar[n-1-index];
+		
+	}
+
+}
+
+int main(){
+	int vet[5] = {1,2,3,4,5};
+	inverte (vet, 5);
+	
+	for(int count = 0; count < 5; count++){
+		printf("%d \t", vet[count]);	
+	}
+}
+ 	
+
+
+
